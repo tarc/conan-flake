4ef6e13087e5181ca1c2eb7649fb9e7213a57e7e
conan-flake CI <ci@conan-flake.invalid>
2026-08-17 23:56:55 +0000

    Publish the documentation site built from 7702075

--- searcher-09f2665d.js
+++ searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-2e39c903.js',
+                path_to_root + 'searchindex-d827e241.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
